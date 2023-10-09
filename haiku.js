d2f572236da4cbc10caca39e651b192724b36b55
Lazarus <27408@ma-web.nl>
2023-10-09 09:45:39 +0200

    jamaleditver2.0

--- haiku.js
+++ haiku.js
@@ -1,17 +1,22 @@
 
 
-#plak hier de haiku functions onder elkaar
+//plak hier de haiku functions onder elkaar
 
 console.log("From time to time")
 console.log("The clouds give rest")
 console.log("To the moon-beholders.")
+
 console.log("Over-ripe sushi, The Master Is full of regret.")
 console.log("Consider me As one who loved poetry And persimmons.")
 console.log("In the cicada's cry No sign can foretell How soon it must die.")
 
+console.log("An old silent pond...")
+console.log("A frog jumps into the pond,")
+console.log("splash! Silence again.")
 
 
-#laat de code hieronder met rust
+
+//laat de code hieronder met rust
 Haiku1()
 Haiku2()
 Haiku3()
