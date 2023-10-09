e2cefb83bfbeee26dd2a5fbfbd6ba280b9f457ed
Lazarus <27408@ma-web.nl>
2023-10-09 09:59:02 +0200

    jamalver3.0

--- haiku.js
+++ haiku.js
@@ -2,19 +2,34 @@
 
 //plak hier de haiku functions onder elkaar
 
-console.log("From time to time")
-console.log("The clouds give rest")
-console.log("To the moon-beholders.")
+function Haiku1(){
+    console.log("haiku by Kobayahsi Issa")
+    console.log("The wren")
+    console.log("Earns his living")
+    console.log("Noiselessly.")
+}
+
+function Haiku2(){
+    console.log("haiku by matsuo bashō")
+    console.log("From time to time")
+    console.log("The clouds give rest")
+    console.log("To the moon-beholders.")
+}
+
 
 console.log("Over-ripe sushi, The Master Is full of regret.")
 console.log("Consider me As one who loved poetry And persimmons.")
 console.log("In the cicada's cry No sign can foretell How soon it must die.")
 
-console.log("An old silent pond...")
-console.log("A frog jumps into the pond,")
-console.log("splash! Silence again.")
+function Haiku3(){
+    console.log("haiku by matsuo basho")
+    console.log("An old silent pond...")
+    console.log("A frog jumps into the pond,")
+    console.log("splash! Silence again.")
 
-function HaikuX(){
+}
+
+function Haiku4(){
     console.log("haiku by:","Troy");
     console.log("Winter seclusion - Listening");
     console.log("that evening, ");
