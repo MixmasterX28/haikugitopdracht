02d7c5118cf8a5ee7be6946786d68da8b10d0907
troyma2007 <146706129+troyma2007@users.noreply.github.com>
2023-10-09 09:40:34 +0200

    Update haikus

--- haiku.js
+++ haiku.js
@@ -5,6 +5,10 @@
 console.log("From time to time")
 console.log("The clouds give rest")
 console.log("To the moon-beholders.")
+console.log("Over-ripe sushi, The Master Is full of regret.")
+console.log("Consider me As one who loved poetry And persimmons.")
+console.log("In the cicada's cry No sign can foretell How soon it must die.")
+
 
 
 #laat de code hieronder met rust
